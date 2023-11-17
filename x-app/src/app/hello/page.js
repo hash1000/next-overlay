c7231c3c536843cadefdcd8179fb93180c9a5938
hash1000 <hashirmughal1000@gmail.com>
2023-11-17 21:48:55 +0500

    change in hello page

--- x-app/src/app/hello/page.js
+++ x-app/src/app/hello/page.js
@@ -1,6 +1,7 @@
 export default function Hello() {
   return<>
   <div>About</div>
-  <h1>hello i am hashir mehmood</h1>
+  <h1>hello i am hashir mehmood feature</h1>
+  <h1>hello this is for feature 2</h1>
   </> 
 }

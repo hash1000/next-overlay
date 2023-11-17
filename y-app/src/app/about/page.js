dcb51c50f457e380c063d6a0fde7d6ddb2e62cfd
hash1000 <hashirmughal1000@gmail.com>
2023-11-17 22:16:19 +0500

    change in yapp about

--- y-app/src/app/about/page.js
+++ y-app/src/app/about/page.js
@@ -6,6 +6,7 @@ export default function AboutPage() {
     <main className={styles.main}>
         <div>
         <h1>About Us</h1>
+        <h1>branch merge feature 2</h1>
         
         </div>
         </main>
